90927df3864678677b4300b18f92ac0ed1ac226b
Henrique Silva Chacorowski <henrique.ti@grupointegrado.br>
2021-03-16 22:54:28 -0300

    Alterações aula 16/03/2021 adicionando certificado para conexao https e cors

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const usuario = require('./src/routes/usuario.js');
 const nota = require('./src/routes/nota.js');
 const checklist = require('./src/routes/checklist.js');
 const tag = require('./src/routes/tag.js');
-
+const fs = require('fs');
+const https = require('https');
 const app = express();
+// const cors = require('cors');
 const port = 3000;
+const portHttps = 443;
+
+// app.use(
+//   cors({
+//     origin: ['https://localhost:8080']
+//   })
+// );
 
 app.use(bodyParser.json());
 
@@ -15,6 +24,17 @@ app.use('/nota', nota);
 app.use('/checklist', checklist);
 app.use('/tag', tag);
 
+const key = fs.readFileSync('src/certs/localhost-key.pem');
+const cert = fs.readFileSync('src/certs/localhost.pem');
+
+const credentials = { key, cert };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(portHttps, () => {
+  console.log(`API rodando seguramente na porta ${portHttps}`)
+})
+
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
